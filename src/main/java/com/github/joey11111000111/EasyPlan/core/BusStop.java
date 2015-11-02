2d23541e4e6f6abc021123f10e0ac9a5c937da61
Kovács Tamás <tommy1991@vipmail.hu>
2015-11-02 18:30:44 +0100

    starting the test of the xml reader functions

--- src/main/java/com/github/joey11111000111/EasyPlan/core/BusStop.java
+++ src/main/java/com/github/joey11111000111/EasyPlan/core/BusStop.java
@@ -8,9 +8,7 @@ import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by joey on 2015.11.01..
@@ -21,6 +19,7 @@ public class BusStop {
     static final BusStop[] allStops;
 
     static {
+        List<BusStop> allStopsList = new LinkedList<BusStop>();
         try {
             // get the 'city.xml' file as an InputStream
             InputStream is = BusStop.class.getResourceAsStream("city.xml");
@@ -32,26 +31,56 @@ public class BusStop {
             Document document = builder.parse(is);
             document.getDocumentElement().normalize();
 
+            // create the bus station
+            NodeList stationNodes = document.getElementsByTagName("bus_station");
+            // there is only one element with the above tag name
+            Element stationElement = (Element)stationNodes.item(0);
+            allStopsList.add(createBusStopObject(stationElement));
+
+            // create all the bus stops
+            NodeList busStops = document.getElementsByTagName("bus_stop");
+            for (int i = 0; i < busStops.getLength(); i++) {
+                Element currentStop = (Element)busStops.item(i);
+                allStopsList.add(createBusStopObject(currentStop));
+            }
         }
         catch (Exception e) {
             // TODO: log the exception
             System.exit(1);
         }
+        allStops = allStopsList.toArray(new BusStop[0]);
     }//static
 
-    private static Map<Integer, Integer> getAllReachablesOf(Element element) {
+
+    private static BusStop createBusStopObject(Element bsElement) {
+        int id, x, y;
+        // get id
+        id = getNumericContentOfTag(bsElement, "id");
+        // get positions
+        Element posNode = (Element)bsElement.getElementsByTagName("position").item(0);
+        x = getNumericContentOfTag(posNode, "x");
+        y = getNumericContentOfTag(posNode, "y");
+
+        Map<Integer, Integer> reachables = getAllReachablesOf(bsElement);
+        return new BusStop(id, x, y, reachables);
+    }
+
+    private static Map<Integer, Integer> getAllReachablesOf(Element busStop) {
         Map<Integer, Integer> reachables = new HashMap<Integer, Integer>();
-        NodeList connections = element.getElementsByTagName("connection");
+        NodeList connections = busStop.getElementsByTagName("connection");
 
         // iterate through the list and fill the map
         for (int i = 0; i < connections.getLength(); i++) {
+            Element connection = (Element)connections.item(i);
+            int refId = getNumericContentOfTag(connection, "refid");
+            int travelTime = getNumericContentOfTag(connection, "travel_time");
+            reachables.put(refId, travelTime);
         }
 
-        return null;
+        return Collections.unmodifiableMap(reachables);
     }
 
-    private static int getNumericContentOfTag(Node node, String tagName) {
-        Element parent = (Element)node;
+    private static int getNumericContentOfTag(Element parent, String tagName) {
         NodeList tags = parent.getElementsByTagName(tagName);
         // there is only one appearance of a certain tag in a connection element, so the first one is the needed
         Element neededElement = (Element)tags.item(0);
@@ -105,6 +134,18 @@ public class BusStop {
 
     public boolean isStationReachable() {
         return reachableStops.containsKey(0);
+    }
+
+    public String toString() {
+        String msgBase = "id : " + id + " x: " + x + "y: " + y
+                + " Reachables: ";
+        StringBuilder sb = new StringBuilder(msgBase);
+        for (Map.Entry<Integer, Integer> entry : reachableStops.entrySet()) {
+            sb.append("   ").append("id: ").append(entry.getKey())
+                    .append(" travel time: ").append(entry.getValue())
+                    .append(System.getProperty("line.separator"));
+        }
+        return sb.toString();
     }
 
 
--- src/main/java/com/github/joey11111000111/EasyPlan/core/Core.java
+++ src/main/java/com/github/joey11111000111/EasyPlan/core/Core.java
@@ -4,4 +4,7 @@ package com.github.joey11111000111.EasyPlan.core;
  * Created by joey on 2015.11.01..
  */
 public class Core {
+    public static void main(String[] args) {
+
+    }
 }//class
