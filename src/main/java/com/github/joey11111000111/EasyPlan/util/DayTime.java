5131f75ce17731b1cce5ed6e7d105a22d015adca
Kovács Tamás <tommy1991@vipmail.hu>
2015-11-14 16:42:49 +0100

    added javadoc to the 'util' package classes

--- src/main/java/com/github/joey11111000111/EasyPlan/util/DayTime.java
+++ src/main/java/com/github/joey11111000111/EasyPlan/util/DayTime.java
@@ -3,14 +3,25 @@ package com.github.joey11111000111.EasyPlan.util;
 import java.io.Serializable;
 
 /**
- * Created by joey on 2015.11.06..
+ * The DayTime class represents the time of one day in HH:MM format,
+ * where HH is in range 0-23 and MM is in range 0-59
  */
 public class DayTime implements Serializable {
+
     static final long serialVersionUID = 0L;
 
     private int hours;
     private int minutes;
 
+    /**
+     * Creates a DayTime instance by converting the given minutes into a HH:MM format.
+     * The minutes must be in range of one day or it can overflow when the
+     * canOverflow permits it. Underflow is never permitted.
+     * @param minutes the day-time to represent in the form of minutes, in range 0 - 1439
+     * @param canOverflow decides whether the overflow is permitted or not
+     * @throws IllegalArgumentException - when the 'minutes' is less then 0 or
+     * more then 1439 and the 'canOverflow' is false
+     */
     public DayTime(int minutes, boolean canOverflow) {
         if (!canOverflow)
             if (minutes < 0 || minutes > (23 * 60 + 59))
@@ -20,32 +31,66 @@ public class DayTime implements Serializable {
         this.minutes = minutes - hours * 60;
         this.hours %= 24;
     }
+
+    /**
+     * Creates a DayTime instance by converting the given minutes into a HH:MM format.
+     * @param minutes the day-time to represent in the form of minutes, must be in range 0 - 1439
+     * @throws IllegalArgumentException - when the 'minutes' is out of the range 0 - 1439
+     */
     public DayTime(int minutes) {
         this(minutes, false);
     }
 
+    /**
+     * Creates a new DayTime instance based on the given parameters
+     * @param hours the hour part of the time, must be in range 0 - 23
+     * @param minutes the minute part of the time, must be in range 0 - 59
+     * @throws IllegalArgumentException - when the 'minutes' is out of the range 0 - 1439
+     */
     public DayTime(int hours, int minutes) {
         setHours(hours);
         setMinutes(minutes);
     }
 
+    /**
+     * Creates a new DayTime instance that represents the exact same time as the given instance.
+     * The created object is a deep copy of the given object
+     * @param rhs the object that will be copied
+     */
     public DayTime(DayTime rhs) {
         hours = rhs.getHours();
         minutes = rhs.getMinutes();
     }
 
-
+    /**
+     * Returns the hour part of the represented time
+     * @return the hour part of the represented time
+     */
     public int getHours() {
         return hours;
     }
+    /**
+     * Returns the minute part of the represented time
+     * @return the minute part of the represented time
+     */
     public int getMinutes() {
         return minutes;
     }
 
+    /**
+     * Converts the represented time into minute and returns the result
+     * @return the represented time converted to minutes (hours * 24 + minutes)
+     */
     public int getTimeAsMinutes() {
         return hours * 60 + minutes;
     }
 
+    /**
+     * Sets the hour part of the time to the given value
+     * @param hours the new hour part of the time, must be in range 0 - 23
+     * @return true, if the contained hour part was modified
+     * @throws IllegalArgumentException - if the 'hours' parameter is out of range
+     */
     public boolean setHours(int hours) {
         if (hours < 0 || hours > 23)
             throw new IllegalArgumentException("hours must be in range [0, 23]");
@@ -55,6 +100,12 @@ public class DayTime implements Serializable {
         return true;
     }
 
+    /**
+     * Sets the minute part of the time to the given value
+     * @param minutes the new minute part of the time, must be in range 0 - 23
+     * @return true, if the contained hour part was modified
+     * @throws IllegalArgumentException - if the 'hours' parameter is out of range
+     */
     public boolean setMinutes(int minutes) {
         if (minutes < 0 || minutes > 59)
             throw new IllegalArgumentException("minutes must be in range [0, 59]");
@@ -64,7 +115,19 @@ public class DayTime implements Serializable {
         return true;
     }
 
+    /**
+     * Returns the represented time in a HH:MM format
+     * @return a string representation of the time in HH:MM format
+     */
     public String toString() {
-        return "" + hours + ':' + minutes;
+        StringBuilder sb = new StringBuilder();
+        if (hours < 10)
+            sb.append(0);
+        sb.append(hours).append(':');
+        if (minutes < 10)
+            sb.append(0);
+        sb.append(minutes);
+
+        return sb.toString();
     }
 }//class
--- src/main/java/com/github/joey11111000111/EasyPlan/util/OpenLinkedList.java
+++ src/main/java/com/github/joey11111000111/EasyPlan/util/OpenLinkedList.java
@@ -1,10 +1,22 @@
 package com.github.joey11111000111.EasyPlan.util;
 
 /**
- * Created by joey on 2015.11.03..
+ * The OpenLinkedList class represents a linked list data structure, with
+ * the capability of appending or removing a part chain to or from the end of the list.
+ * It is "open", because it gives a little insight to the inner structure, in the form of
+ * Nodes. Besides the stored data, the wrapping node can also be received, with all the possible
+ * following nodes. The received part chain is removed from the list, and also unmodifiable,
+ * so it is safe to use. This chain can also be appended to the list at a later time, independent
+ * of the current state of the list. All data modifications happen at the end of the list.
  */
 public class OpenLinkedList<E> {
 
+    /**
+     * Represents one link in a linked list, with references to the next and previous
+     * links, and the stored data of the given type. After creation, all instances are
+     * unmodifiable outside of the container class.
+     * @param <E> the type of the stored element
+     */
     public static class Node<E> {
         private E element;
         private Node<E> next;
@@ -20,24 +32,47 @@ public class OpenLinkedList<E> {
         }
 
         // iteration methods --------------
+
+        /**
+         * Returns true, when there is at least one following node in the cain
+         * @return true, when there is at least one following node in the chain
+         */
         public boolean hasNext() {
             return next != null;
         }
+        /**
+         * Returns true, when there is at least one previous node in the cain
+         * @return true, when there is at least one previous node in the chain
+         */
         public boolean hasPrevious() {
             return previous != null;
         }
 
+        /**
+         * Returns the following node in the chain.
+         * @return the next node in the chain
+         * @throws IllegalStateException when the current is the last node in the chain
+         */
         public Node<E> next() {
             if (hasNext())
                 return next;
             throw new IllegalStateException("no next element");
         }
+        /**
+         * Returns the previous node in the chain.
+         * @return the previous node in the chain
+         * @throws IllegalStateException when the current is the first node in the chain
+         */
         public Node<E> previous() {
             if (hasPrevious())
                 return previous;
             throw new IllegalStateException("no previous element");
         }
 
+        /**
+         * Returns the stored element
+         * @return the stored element of the given type
+         */
         public E getElement() {
             return element;
         }
@@ -47,19 +82,34 @@ public class OpenLinkedList<E> {
     private Node<E> tail;
     private int size;
 
+    /**
+     * Creates an empty list
+     */
     public OpenLinkedList() {
         head = tail = null;
         size = 0;
     }
 
+    /**
+     * Returns true, when the list hasn't got any elements.
+     * @return true, when the list is empty
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Returns the number of stored elements
+     * @return the number of stored elements
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Creates a wrapping node for the element, and appends that node to the end of the list.
+     * @param element the element to store at the end of the list
+     */
     public void append(E element) {
         Node<E> newNode = new Node<E>(element, null, tail);
         if (tail == null)
@@ -71,6 +121,11 @@ public class OpenLinkedList<E> {
         size++;
     }
 
+    /**
+     * Appends the given node to the end of the list. If the node has following nodes (it is a chain)
+     * than the following elements are added too.
+     * @param chain the chain to add to the end of the list
+     */
     public void appendChain(Node<E> chain) {
         if (head == null) {
             head = chain;
@@ -90,10 +145,21 @@ public class OpenLinkedList<E> {
         size += chainLinkNum;
     }
 
+    /**
+     * Returns true, when there is an element at the specified index
+     * @param index the index to specify the position of an element
+     * @return true, when the index is not negative and smaller than the
+     * number of elements in the list
+     */
     public boolean validIndex(int index) {
         return index >= 0 && index < size;
     }
 
+    /**
+     * Removes the last node from the list and returns it.
+     * @return the last node of the list
+     * @throws IllegalStateException when the list is empty
+     */
     public Node<E> removeLast() {
         if (isEmpty())
             throw new IllegalStateException("the list is empty");
@@ -133,6 +199,15 @@ public class OpenLinkedList<E> {
         }
     }
 
+    /**
+     * Removes the node at the specified index, with all the following nodes,
+     * and returns this node. The index of the last element in the modified list
+     * will be 'index - 1'.
+     * @param index specifies which node of the list will be removed by its index
+     * @return the removed node, which can have following nodes (it can be a chain)
+     * @throws IllegalStateException when the list is empty, thus there aren't any elements to remove
+     * @throws IndexOutOfBoundsException when the specified index is out of range
+     */
     public Node<E> removeChainFrom(int index) {
         if (isEmpty())
             throw new IllegalStateException("the list is empty");
@@ -154,6 +229,14 @@ public class OpenLinkedList<E> {
         return removeChainFrom(node);
     }
 
+    /**
+     * Removes and returns the last node that contains the specified element. All the following
+     * elements are removed and returned, as a chain.
+     * @param element the last node will be removed that contains an element that is equal with this
+     * @return the node (possibly chain) that contains the given element and has the highest index
+     * @throws IllegalArgumentException when the list is empty
+     * @throws IllegalArgumentException when the given element is not contained by any of the nodes
+     */
     public Node<E> removeChainFrom(E element) {
         if (isEmpty())
             throw new IllegalStateException("list is empty");
@@ -169,6 +252,9 @@ public class OpenLinkedList<E> {
         throw new IllegalArgumentException("the element to remove is not in the list");
     }
 
+    /**
+     * Removes all elements of the list
+     */
     public void clear() {
         if (isEmpty())
             return;
@@ -176,9 +262,17 @@ public class OpenLinkedList<E> {
         size = 0;
     }
 
+    /**
+     * Returns the first node of the list
+     * @return the first node of the list
+     */
     public Node<E> getHead() {
         return head;
     }
+    /**
+     * Returns the last node of the list
+     * @return the last node of the list
+     */
     public Node<E> getTail() {
         return tail;
     }
